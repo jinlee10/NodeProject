a6d66d56b9e01599e3134a371db86f1cb7a1dd32
jinlee10 <jinleexjinlee@gmail.com>
2017-07-31 17:21:16 +0900

    목록불러오기랑 서버-클라 net으로 통신하기

--- NodeProject2/day2_0731/l_client.js
+++ NodeProject2/day2_0731/l_client.js
@@ -2,14 +2,30 @@
  * 
  */
 
+// 7교시
 
+const net = require('net');
 
+var ip = '192.168.205.122';
+var port = 30033;
 
+var client = new net.Socket();
 
+client.connect(port, ip, function(){
+	console.log('successfully connected');
+	client.write('하이');
+});
 
+client.on('data', function(data){
+	//읽었을때 data가 진짜 오른쪽 으로 간다
+	console.log('receive data: ' + data);
+//	client.destroy();
+});
 
-
-
+client.on('end', function(){
+	//끝나고나면 얘가실행됩니다.
+	console.log('connection closed');
+});
 
 
 
--- NodeProject2/day2_0731/l_server.js
+++ NodeProject2/day2_0731/l_server.js
@@ -2,10 +2,29 @@
  * 
  */
 
+// 7교시
 
+const net = require('net');
 
+var server = net.createServer(function(socket){ //소켓이 필요해
+	console.log(socket.remoteAddress);//소켓이 가진 remoteAddress
+	
+	socket.on('data', function(data){//rederThread라고바도된다
+		console.log('read data: ' + data);
+		socket.write('echo : ' + data);
+//		socket.write();
+	});	
+	socket.on('end', function(){
+		
+		//end가되면 callback함수를 실행하는것을 등록
+		console.log('connection closed');
+	});
+	
+});
 
-
+//상대방의 접속을 기다린다
+server.listen(30033);//인자로 포트를준다
+console.log('서버 스타트');
 
 
 
